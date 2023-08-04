f54289f6b9cb66c69940c5b7e4bf30acb9667c0f
eugenioclrc <eugenioclrc@gmail.com>
2023-08-04 16:02:05 -0300

    Aguanta loco, call #2

--- test/Huffplug.t.sol
+++ test/Huffplug.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import {Test, console2, Vm} from "forge-std/Test.sol";
 import {compile, create} from "huff-runner/Deploy.sol";
 import {IERC721} from "forge-std/interfaces/IERC721.sol";
+import {TokenRenderer} from "src/TokenRenderer.sol";
 
 using { compile } for Vm;
 using { create } for bytes;
@@ -21,7 +22,13 @@ contract CounterTest is Test {
     IERC721Metadata public huffplug;
 
     function setUp() public {
+        TokenRenderer renderer = new TokenRenderer("https://huffplug.com/");
         huffplug = IERC721Metadata(vm.compile("src/Huffplug.huff").create({value: 0}));
+    }
+
+    function testRenderer() public {
+        TokenRenderer renderer = new TokenRenderer("https://huffplug.com/");
+        assertEq(renderer.tokenURI(3), "https://huffplug.com/3.json");
     }
 
     function testMint() public {
@@ -32,7 +39,17 @@ contract CounterTest is Test {
     function testMetadata() public {
         assertEq(huffplug.symbol(), "UwU");
         assertEq(huffplug.name(), "Buttpluggy");
-        console2.log(huffplug.tokenURI(3));
     }
 
+    function testTokenUri() public {
+        assertEq(huffplug.tokenURI(3), "https://huffplug.com/3.json");
+        assertEq(huffplug.tokenURI(10), "https://huffplug.com/10.json");
+        vm.expectRevert();
+        huffplug.tokenURI(0);
+
+        vm.expectRevert();
+        huffplug.tokenURI(1025);
+
+        assertEq(huffplug.tokenURI(1024), "https://huffplug.com/1024.json");
+    }
 }
