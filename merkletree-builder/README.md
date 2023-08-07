eb0221f02d3b7dd14ac6705c78560a21f3c8646e
eugenioclrc <eugenioclrc@gmail.com>
2023-08-06 21:21:39 -0300

    merkleetree hello world

---
+++ merkletree-builder/README.md
@@ -0,0 +1,3 @@
+# Whitelist address based on poap from webtr3s events
+
+- https://poap.gallery/event/126806
---
+++ merkletree-builder/index.js
@@ -0,0 +1,26 @@
+
+const fs = require('fs');
+const { MerkleTree } = require('merkletreejs');
+const {keccak256, isAddress} = require('viem');
+
+const whitelisted = String(fs.readFileSync('./whitelist.txt')).split("\n").map(x => x.trim()).filter(isAddress);
+
+const padBuffer = (addr) => {
+  return Buffer.from(addr.substr(2).padStart(32*2, 0), 'hex')
+}
+
+const leaves = whitelisted.map(account => padBuffer(account))
+const tree = new MerkleTree(leaves, keccak256, { sort: true })
+const merkleRoot = tree.getHexRoot()
+
+console.log({merkleRoot});
+
+console.log(tree.toString())
+
+
+
+  /*
+const leaves = ['a', 'b', 'c'].map(x => SHA256(x))
+const tree = new MerkleTree(leaves, SHA256)
+const root = tree.getRoot().toString('hex')
+*/
