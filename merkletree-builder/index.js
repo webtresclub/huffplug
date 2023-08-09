903bd2747b97e52c7566ce7bc70f11ec12ffcdb0
eugenioclrc <eugenioclrc@gmail.com>
2023-08-09 14:56:06 -0300

    log all proofs

--- merkletree-builder/index.js
+++ merkletree-builder/index.js
@@ -14,12 +14,19 @@ const tree = new MerkleTree(leaves, keccak256, { sort: true })
 const merkleRoot = tree.getHexRoot()
 
 console.log({merkleRoot});
-const merkleProof = tree.getHexProof(padBuffer(whitelisted[0]))
-console.log({merkleProof});
+
 
 console.log(tree.toString())
 
 
+const proofs = whitelisted.map(wallet => {
+  return {
+    wallet,
+    proof: tree.getHexProof(padBuffer(wallet))
+  }
+});
+
+console.log(proofs);
 
 
   /*
