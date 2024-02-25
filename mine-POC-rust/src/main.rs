820c5ec35001d92502f57558357824b519ed9acc
Eugenio <eugenioclrc@gmail.com>
2024-02-25 01:05:29 -0300

    multi thread mining

--- mine-POC-rust/src/main.rs
+++ mine-POC-rust/src/main.rs
@@ -1,22 +1,59 @@
 extern crate ethers;
 
+use std::env;
+
 use ethers::types::{self, H256, H160, U256};
 use ethers::abi::{self, Token, Address};
 use ethers::utils::{self, keccak256};
 
-use hex::encode as hex_encode; // Added for hex encoding
-use hex::decode as hex_decode;//
+use hex::encode as hex_encode;
+use hex::decode as hex_decode;
 
-use rand::Rng;
-
+use rand::{self, Rng};
+use std::sync::mpsc;
+use std::thread;
 use std::{fmt::Write, num::ParseIntError, time::Instant};
 
-
 fn main() {
-    let user: H160 = "0x6CA6d1e2D5347Bfab1d91e883F1915560e09129D".parse().unwrap();
-    let current_salt: H256 = "0x851e34d5f1de39b1758b00f0040ae4ab4f3bc8c5631b4a8463144022de2cf428".parse().unwrap();
-    let difficulty = 9;
-    
+    /*
+    let user: H160 = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045".parse().unwrap();
+    let current_salt: H256 = "0x5a711436e0ac9c67ddf36e0fbfe7563b84b66557d9645db22a1feb55e57c2bf8".parse().unwrap();
+    let difficulty = 8;
+    let num_threads = 4; // Adjust based on your hardware
+
+    */
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 5 {
+        eprintln!("Usage: ./mine_POC_rust <user address> <current salt> <difficulty> <number of threads>");
+        std::process::exit(1);
+    }
+
+    let user: H160 = args[1].parse().expect("Invalid address");
+    let current_salt: H256 = args[2].parse().expect("Invalid salt");
+    let difficulty: usize = args[3].parse().expect("Invalid difficulty");
+    let num_threads: usize = args[4].parse().expect("Invalid number of threads");
+
+    // Explicitly specify the message type for the channel
+    let (tx, rx): (mpsc::Sender<(Vec<u8>, Vec<u8>)>, mpsc::Receiver<(Vec<u8>, Vec<u8>)>) = mpsc::channel();
+
+    for thread_number in 0..num_threads {
+        let tx = tx.clone();
+        
+        thread::spawn(move || {
+            hash_loop(user, current_salt, difficulty, tx, thread_number);
+        });
+    }
+
+    if let Ok((nonce, salt)) = rx.recv() {
+        println!("Success with nonce: 0x{:}", hex_encode(&nonce));
+        println!("Hash output: 0x{:}", hex_encode(&salt));
+    }
+}
+
+
+fn hash_loop(user: H160, current_salt: H256, difficulty: usize, tx: mpsc::Sender<(Vec<u8>, Vec<u8>)>, thread_n: usize) {
     loop {
         let start = Instant::now(); // Start timing
 
@@ -32,31 +69,21 @@ fn main() {
         let mut encoded_with_nonce = encoded.clone();
         encoded_with_nonce.extend_from_slice(&_n); // Append the nonce bytes directly
             
-        for i in 0..1_000_000 {            
+        for i in 31..1_000_000 {            
             let nonce_index = base_len + (i % 32);
             encoded_with_nonce[nonce_index] = rng.gen_range(0..=255);
             
-
-            // let encoded = abi::encode_packed(&[Token::Address(user), Token::FixedBytes(current_salt.0.to_vec()),Token::Uint(nonce)]).unwrap();
             let salt = keccak256(&encoded_with_nonce);
-            // println!("hash: 0x{:?}", hex_encode(&salt.to_vec()));
-           // println!("hash: {:?}", nonce_index - base_len);
             
             if leading_zeros(&salt, difficulty) {
-                println!("seed: 0x{:}", hex_encode(&encoded_with_nonce[base_len..].to_vec()));
-                println!("hash output: 0x{:}", hex_encode(&salt.to_vec()));
-
-                // this will exit
+                tx.send((encoded_with_nonce[base_len..].to_vec(), salt.to_vec())).unwrap();
                 return;
-                // break;
             }
             
         }
-
-
         let elapsed = start.elapsed().as_secs_f32(); // Calculate elapsed time in seconds
         let hashes_per_second = 1_000_000f32 / elapsed;
-        println!("loop, Hashes per second: {}", hashes_per_second);
+        println!("thread {}, Hashes per second: {}", thread_n, hashes_per_second);
     }
 }
 
